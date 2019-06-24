abb428543f67faea308537b7272b3bea28cba956
Ari Mendelow <ari@mendelow.me>
2019-06-23 22:18:55 -0500

    Move main method on top; add newlines to input dialogs

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,3 +1,14 @@
+
+def main():
+	print("Welcome to Tic Tac Toe, a demo CL based game by Ari Mendelow!")
+	p1 = input("What symbol do you want to use to represent player 1? (Default X)\n")
+	p2 = input("What symbol do you want to use to represent player 2? (Default O)\n")
+	if (p1 != p2 and p1 != "" and p2 != ""):
+		board = Board(p1, p2)
+	else:
+		board = Board()
+	print(f"Player 1 is {board.p1} and player 2 is {board.p2}")
+
 class Board:
 	def __init__(self, p1='X', p2='O'):
 		self.p1 = p1
@@ -11,16 +22,6 @@ class Board:
 		print(f"2  {self.board[3]} | {self.board[4]} | {self.board[5]}")
 		print("  ---+---+---")
 		print(f"3  {self.board[6]} | {self.board[7]} | {self.board[8]}")
-
-def main():
-	print("Welcome to Tic Tac Toe, a demo CL based game by Ari Mendelow!")
-	p1 = input("What symbol do you want to use to represent player 1? (Default X)")
-	p2 = input("What symbol do you want to use to represent player 2? (Default O)")
-	if (p1 != p2 and p1 != "" and p2 != ""):
-		board = Board(p1, p2)
-	else:
-		board = Board()
-	print(f"Player 1 is {board.p1} and player 2 is {board.p2}")
 
 if __name__ == "__main__":
 	main()
