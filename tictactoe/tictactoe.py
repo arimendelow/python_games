04e424c0e36bc8e0bec7015acaec02a4143e68df
Ari Mendelow <ari@mendelow.me>
2019-06-23 22:32:08 -0500

    Add method for checking if the game is over

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -7,7 +7,9 @@ def main():
 		board = Board(p1, p2)
 	else:
 		board = Board()
-	print(f"Player 1 is {board.p1} and player 2 is {board.p2}")
+	print("Let's play!")
+	board.isGameOver()
+
 
 class Board:
 	def __init__(self, p1='X', p2='O'):
@@ -22,6 +24,26 @@ class Board:
 		print(f"2  {self.board[3]} | {self.board[4]} | {self.board[5]}")
 		print("  ---+---+---")
 		print(f"3  {self.board[6]} | {self.board[7]} | {self.board[8]}")
+	
+	def isGameOver(self):
+		winningCombos = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6]
+		]
+		for combo in winningCombos:
+			[a, b, c] = combo
+			# Check if match
+			if (self.board[a] != ' ' and self.board[a] == self.board[b] and self.board[a] == self.board[c]):
+				# Return the glyph of the winner
+				return self.board[a]
+		# No winner yet
+		return None
 
 if __name__ == "__main__":
 	main()
